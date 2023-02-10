51cda6e4a6584b00071d725cc80ffdb7128a6ee8
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-02-10 20:34:03 +0530

    Second Program

---
+++ Basic Programs/02 Accept And Print Value Of Given Variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int No = 0;
+
+    cout << endl<<"\t Enter Value Of No => ";
+    cin >>No;
+
+    cout << endl<<"\t Value Of No => "<<No;
+
+    getch();
+    return 0;
+}
