51bf1cede279483e4946eafbc5d4804f4c899753
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-02-19 22:11:20 +0530

    Modulation Of Two Numbers

---
+++ Arithmetic Programs/05_Modulation_of_2_numbers.cpp
@@ -0,0 +1,20 @@
+//Compiler version g++ 6.3.0
+using namespace std;
+#include <iostream>
+#include <conio.h>
+
+
+int main()
+{
+  
+    int No1,No2;
+    
+    
+    cout<<endl<<"\t Enter 2 Numbers => ";
+    cin>>No1>>No2;
+    
+    cout<<endl<<"\t"<< No1 <<" % "<< No2<<" = "<< No1 % No2;
+    
+    getch();
+    return 0;
+}
