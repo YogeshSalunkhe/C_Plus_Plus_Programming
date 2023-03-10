8bcd6ee5b7036e19ffc48773f3c78d5d709daa95
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-03-10 21:44:19 +0530

    if Statement in cpp

---
+++ Control Statements/01_if_Statement.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+    cout << "\n Enter A Number => ";
+    cin >> No;
+
+    if(No > 0)
+    {
+        cout<<"\n Given Number Is Positive Number.";
+    }
+
+    getch();
+    return 0;
+}
